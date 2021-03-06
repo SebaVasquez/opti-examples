907e7892aa8a57e659f50bd72c6beb110d25bf0d
savasquez <savasquez@uc.cl>
2021-03-06 16:04:41 -0300

    added: args parser

--- src/simplex/run.py
+++ src/simplex/run.py
@@ -1,15 +1,27 @@
 from argparse import ArgumentParser
 from time import time
+import importlib
 import os, sys
 sys.path.append(os.getcwd())
 
-from src.simplex.constants import consts_1, consts_2, consts_3
 from src.simplex.classes.instance import SimplexInstance as Instance
 from src.simplex.classes.simplex import Simplex
 
 def run():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-i', '--instance', 
+        type=str,
+        help='Instance data obtained from ./src/simplex/constants.py')
+    
+    args = parser.parse_args()
+    import_path = 'src.simplex.constants' 
+    data = getattr(importlib.import_module(import_path), args.instance or 'consts_1')
+    print('Solving LP with the following data:', data)
+
+    # Algorithm's execution
     t0 = time()
-    instance = Instance(consts_3)
+    instance = Instance(data)
     simplex = Simplex(instance)
     iters, optimal = simplex.run()
     print('\n\nFinished after {} iterations. Time elapsed: {} secs.'.format(iters, round(time() - t0, 2)))
